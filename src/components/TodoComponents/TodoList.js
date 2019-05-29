c6d5c234728743c3121b0a275cc60e005a8040db
vebradev <justinas@vebra.dev>
2019-05-29 17:16:46 +0300

    Remove old code.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,25 +1,13 @@
 import React from "react";
-import Todo from './Todo';
+import Todo from "./Todo";
 
 class TodoList extends React.Component {
   render() {
     return (
       <div>
-        {/* {this.props.todoList.map(item => (
-          <div key={item.id}>{item.task}</div>
-        ))} */}
-
-        {
-            this.props.todoList.map((item) => {
-                return (
-                    <Todo 
-                        key={item.id}
-                        id={item.id}
-                        task={item.task}   
-                    />
-                );
-            })
-        }
+        {this.props.todoList.map(item => {
+          return <Todo key={item.id} id={item.id} task={item.task} />;
+        })}
       </div>
     );
   }
